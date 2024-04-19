f28baf812e72263a36e3cf408ee1eb3bf45e04f5
Nathan McCormack <G00414780@atu.ie>
2024-04-19 11:58:26 +0100

    Added new tables to database

--- gamepass.sql
+++ gamepass.sql
@@ -18,6 +18,69 @@ USE `gamepass`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `address`
+--
+
+DROP TABLE IF EXISTS `address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `address` (
+  `Address_ID` int NOT NULL AUTO_INCREMENT,
+  `Customer_ID` int NOT NULL,
+  `Region` varchar(30) DEFAULT NULL,
+  `Country` varchar(30) DEFAULT NULL,
+  `City` varchar(30) DEFAULT NULL,
+  `Address_line_1` varchar(50) DEFAULT NULL,
+  `Address_line_2` varchar(50) DEFAULT NULL,
+  `Postal_code` varchar(15) DEFAULT NULL,
+  `Last_Update` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Address_ID`),
+  KEY `Customer_ID` (`Customer_ID`),
+  CONSTRAINT `address_ibfk_1` FOREIGN KEY (`Customer_ID`) REFERENCES `customer` (`Customer_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `address`
+--
+
+LOCK TABLES `address` WRITE;
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `Customer_ID` int NOT NULL AUTO_INCREMENT,
+  `First_Name` varchar(20) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `age` int NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `Username` varchar(40) NOT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Gender` varchar(20) NOT NULL,
+  `Date_of_birth` varchar(30) NOT NULL,
+  `Last_Update` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Customer_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `game`
 --
 
@@ -34,6 +97,7 @@ CREATE TABLE `game` (
   `Rating` varchar(10) NOT NULL,
   `Original_Language` varchar(20) NOT NULL,
   `Platform` varchar(50) DEFAULT NULL,
+  `Last_Update` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`GAME_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -44,7 +108,7 @@ CREATE TABLE `game` (
 
 LOCK TABLES `game` WRITE;
 /*!40000 ALTER TABLE `game` DISABLE KEYS */;
-INSERT INTO `game` VALUES (1,'FIFA 23','FOOTBALL GAME',60,124,'19/09/2022','6/10','ENGLISH','XBOX');
+INSERT INTO `game` VALUES (1,'FIFA 22','FOOTBALL GAME',60,124,'19/09/2022','6/10','ENGLISH','XBOX',NULL);
 /*!40000 ALTER TABLE `game` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +121,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-18 14:58:02
+-- Dump completed on 2024-04-19 11:54:57
