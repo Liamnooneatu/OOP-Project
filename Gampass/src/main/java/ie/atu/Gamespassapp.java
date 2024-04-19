0d638525193e74e6e3840511c27ea4081307a4ad
Eoin Fitzpatrick <G00418325@atu.ie>
2024-04-19 13:01:04 +0100

    adding total cost and review

--- Gampass/src/main/java/ie/atu/Gamespassapp.java
+++ Gampass/src/main/java/ie/atu/Gamespassapp.java
@@ -1,41 +1,57 @@
 package ie.atu;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Gamespassapp {
+public class Gamespassapp extends Gamepass{
     public static void main(String[] args) {
         System.out.println("Welcome to the our GamePass");
         System.out.println();
 
+
+        double totalCost = 0.0;
         // perform 1 or more selections
       //  @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
         String choice = "y";
         while (choice.equalsIgnoreCase("y")) {
             System.out.print("Enter the Game you would like to purchase: ");
-            String GamepassCode = sc.nextLine();  // read the product code
+            String GamepassCode = sc.nextLine();
 
-            // get the Product object "p"
+
             Gamepass g = GamepassDB.getGamepass(GamepassCode);
 
             System.out.println();
             if (g != null) {
                 System.out.println("Description \n" + g.toString());
+                totalCost += g.getPrice();
             } else {
                 System.out.println("No product matches this product code.");
             }
 
             System.out.println();
             System.out.println("GamePass count: " + Gamepass.getCount() + "\n");
+            System.out.println("Total Cost so far: " + totalCost);
 
             // see if the user wants to continue
-            System.out.print("Continue? (y/n): ");
+            System.out.print("Continue adding more games (y/n): ");
             choice = sc.nextLine();
             System.out.println();
 
+
         }
+        // Display the final total cost after the user finishes adding games
+        System.out.println("You have bought: " + Gamepass.getCount() + " Games");
+        System.out.println("Final Total Cost: " + totalCost);
+        System.out.println("Thank you for using our GamePass App \nWe hope you enjoyed your experience.");
+        System.out.println();
+
+        System.out.print("could you please rate our Gamepass App out of 10 :) ");
+        String GamepassCode = sc.nextLine();
+        System.out.print("Thank you for your cooperation");
 
     }
+
 }
