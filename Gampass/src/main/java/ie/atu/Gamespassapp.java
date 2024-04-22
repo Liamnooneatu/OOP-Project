9e96d9342cf5189a7ed078426af9d020fbdf152f
Eoin Fitzpatrick <G00418325@atu.ie>
2024-04-22 14:10:25 +0100

    sql and array

--- Gampass/src/main/java/ie/atu/Gamespassapp.java
+++ Gampass/src/main/java/ie/atu/Gamespassapp.java
@@ -3,78 +3,53 @@ package ie.atu;
 import java.sql.*;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Gamespassapp extends Gamepass{
-        public static void main(String[]args) {
-            String url = "jdbc:mysql://localhost:3306/gamepass";
-            String username = "root";
-            String password = "password";
+public class Gamespassapp extends Gamepass {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/gamepass";
+        String username = "root";
+        String password = "password";
 
-            System.out.println("Welcome to the our GamePass");
-            System.out.println();
+        System.out.println("Welcome to our GamePass");
+        System.out.println();
 
-            System.out.println("Please select what platform you would like to Search For:\nA-XBOX\nB-PC\nC-Playstation");
-            Scanner userInput = new Scanner(System.in);
-            String input = userInput.nextLine();
+        // Define the platform options and their corresponding SQL queries
+        String[] platforms = {"XBOX", "PC", "Playstation"};
+        String[] queries = {
+                "SELECT * FROM game WHERE Platform = 'XBOX'",
+                "SELECT * FROM game WHERE Platform = 'PC'",
+                "SELECT * FROM game WHERE Platform = 'Playstation'"
+        };
 
-            if (input.equalsIgnoreCase("A")) {
+        // Display platform options
+        System.out.println("Please select the platform:");
+        for (int i = 0; i < platforms.length; i++) {
+            System.out.println((i + 1) + ". " + platforms[i]);
+        }
 
-                String selectSQL = "SELECT * FROM game WHERE Platform = 'XBOX';";
+        Scanner userInput = new Scanner(System.in);
+        int choice = userInput.nextInt();
 
-                try (Connection connection = DriverManager.getConnection(url, username, password);
-                     Statement statement = connection.createStatement();
-                     ResultSet resultSet = statement.executeQuery(selectSQL)) {
+        if (choice >= 1 && choice <= platforms.length) {
+            String selectSQL = queries[choice - 1]; // Get the corresponding SQL query based on user's choice
 
-                    while (resultSet.next()) {
-                        String Title = resultSet.getString("Title");
-                        String Des = resultSet.getString("Description");
-                        String cost = resultSet.getString("Cost");
-                        String size = resultSet.getString("size_GB");
+            try (Connection connection = DriverManager.getConnection(url, username, password);
+                 Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(selectSQL)) {
 
-                        System.out.println("Title: " + Title + ", Description " + Des + ", Cost: " + cost + ", Size GB: " + size);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                while (resultSet.next()) {
+                    String title = resultSet.getString("Title");
+                    String description = resultSet.getString("Description");
+                    double cost = resultSet.getDouble("Cost");
+                    double sizeGB = resultSet.getDouble("size_GB");
+
+                    System.out.println("Title: " + title + ", Description: " + description + ", Cost: " + cost + ", Size GB: " + sizeGB);
                 }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            else if(input.equalsIgnoreCase("B")){
-                String selectSQL = "SELECT * FROM game WHERE Platform = 'PC';";
-
-                try (Connection connection = DriverManager.getConnection(url, username, password);
-                     Statement statement = connection.createStatement();
-                     ResultSet resultSet = statement.executeQuery(selectSQL)) {
-
-                    while (resultSet.next()) {
-                        String Title = resultSet.getString("Title");
-                        String Des = resultSet.getString("Description");
-                        String cost = resultSet.getString("Cost");
-                        String size = resultSet.getString("size_GB");
-
-                        System.out.println("Title: " + Title + ", Description " + Des + ", Cost: " + cost + ", Size GB: " + size);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            else if(input.equalsIgnoreCase("C")){
-                String selectSQL = "SELECT * FROM game WHERE Platform = 'Playstation';";
-
-                try (Connection connection = DriverManager.getConnection(url, username, password);
-                     Statement statement = connection.createStatement();
-                     ResultSet resultSet = statement.executeQuery(selectSQL)) {
-
-                    while (resultSet.next()) {
-                        String Title = resultSet.getString("Title");
-                        String Des = resultSet.getString("Description");
-                        String cost = resultSet.getString("Cost");
-                        String size = resultSet.getString("size_GB");
-
-                        System.out.println("Title: " + Title + ", Description " + Des + ", Cost: " + cost + ", Size GB: " + size);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+        } else {
+            System.out.println("Invalid choice.");
         }
     }
+}
