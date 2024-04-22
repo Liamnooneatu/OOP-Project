bbd43fd7d9377667cc2a8fbb0210cf06b52f5ea4
Eoin Fitzpatrick <G00418325@atu.ie>
2024-04-22 12:23:32 +0100

    Changing PC

--- Gampass/src/main/java/ie/atu/PC.java
+++ Gampass/src/main/java/ie/atu/PC.java
@@ -12,6 +12,7 @@ public class PC extends Gamepass{
         version =  "PC ";
         count++;
     }
+
     public String getVersion() {
         return version;
     }
@@ -20,46 +21,12 @@ public class PC extends Gamepass{
         this.version = version;
     }
 
-
     @Override
     public String toString() {
         return super.toString() + "The device is a " + version;
     }
 
-    public void main(String[] args)throws SQLException{
 
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/gamepass", "root", "password");
 
-        try{
-            PreparedStatement stmt = conn.prepareStatement("INSERT INTO game (Title,Description,Cost,size_GB,Release_Date,Rating,Original_Language,Platform) VALUES (?,?,?,?,?,?,?,?)");
 
-            Scanner scanner = new Scanner(System.in);
-
-            stmt.setString(1,name);
-            stmt.setString(2,description);
-            stmt.setString(3,cost);
-            stmt.setString(4,sizeGb);
-            stmt.setString(5,releaseDate);
-            stmt.setString(6,rating);
-            stmt.setString(7,language);
-            stmt.setString(8,version);
-            stmt.executeUpdate();
-
-            System.out.println("Insert completed successfully");
-        } catch(SQLException ex){
-            System.out.println("Record insert failed");
-            ex.printStackTrace();
-        }
-        conn.close();
-    }
-
-    private static int getLastInsertId(Connection conn) throws SQLException{
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT LAST_INSERT_ID()");
-        rs.next();
-        int id = rs.getInt(1);
-        rs.close();
-        stmt.close();
-        return id;
-    }
 }
